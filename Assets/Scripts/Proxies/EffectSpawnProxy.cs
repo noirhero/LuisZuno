b3fa4501a73622347a71be68ca9e02c1fc6dfde6
nhlee <noirhero83@gmail.com>
2019-09-18 02:10:37 +0900

    Working effect spawn system.

---
+++ Assets/Scripts/Proxies/EffectSpawnProxy.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2018-2019 TAP, Inc. All Rights Reserved.
+
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[RequiresEntityConversion]
+public class EffectSpawnProxy : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity {
+    public GameObject prefab;
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs) {
+        referencedPrefabs.Add(prefab);
+    }
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+        dstManager.AddComponentData(entity, new EffectSpawnComponent() {
+            preset = conversionSystem.GetPrimaryEntity(prefab)
+        });
+    }
+}

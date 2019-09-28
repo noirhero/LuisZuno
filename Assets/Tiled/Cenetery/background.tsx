08cce9a08a04c15090a91befc4bc8ecd34c532e1
nhlee <noirhero83@gmail.com>
2019-09-28 11:40:17 +0900

    Add new map. (Cenetery)

---
+++ Assets/Tiled/Cenetery/background.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="background" tilewidth="384" tileheight="224" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="384" height="224" source="background.png"/>
+ </tile>
+ <tile id="3">
+  <image width="384" height="123" source="graveyard.png"/>
+ </tile>
+ <tile id="4">
+  <image width="192" height="179" source="mountains.png"/>
+ </tile>
+</tileset>
---
+++ Assets/Tiled/Cenetery/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="ground" tilewidth="16" tileheight="16" tilecount="280" columns="28">
+ <image source="tileset.png" width="448" height="160"/>
+</tileset>

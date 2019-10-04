10507d00385e444ab2951b24cd0cb78854c58e3b
hjkim <khj00767@naver.com>
2019-10-04 14:52:13 +0900

    TargetSystem 코드 정리

--- Assets/Scripts/Systems/TargetSystem.cs
+++ Assets/Scripts/Systems/TargetSystem.cs
@@ -8,48 +8,36 @@ using GlobalDefine;
 
 public class TargetSystem : ComponentSystem {
     protected override void OnUpdate() {
-
-        int chooseIndex = int.MaxValue;
-        int compareIndex = int.MaxValue;
-        float compareDist = float.PositiveInfinity;
-        Vector2 comparePos = Vector2.zero;
+        var chooseIndex = int.MaxValue;
+        var compareIndex = int.MaxValue;
+        var compareDist = float.PositiveInfinity;
+        var comparePos = Vector2.zero;
+        var compareType = EntityType.None;
 
         Entities.ForEach((Entity baseEntity, ref ReactiveComponent baseReactiveComp, ref Translation basePos, ref TargetComponent baseTargetComp) => {
             chooseIndex = int.MaxValue;
             compareIndex = baseEntity.Index;
             compareDist = float.PositiveInfinity;
             comparePos = new Vector2(basePos.Value.x, basePos.Value.y);
+            compareType = baseReactiveComp.type;
 
-            if (baseReactiveComp.type == EntityType.Player) {
-                Entities.ForEach((Entity entity, ref ReactiveComponent reactiveComp, ref Translation pos, ref TargetComponent targetComp) => {
-                    if (reactiveComp.type == EntityType.Player)
-                        return;
+            Entities.ForEach((Entity entity, ref ReactiveComponent reactiveComp, ref Translation pos, ref TargetComponent targetComp) => {
+                if (compareIndex == entity.Index)
+                    return;
 
-                    if (compareIndex == entity.Index)
-                        return;
+                if (compareType == EntityType.Player && reactiveComp.type == EntityType.Player)
+                    return;
 
-                    float distance = Vector2.Distance(comparePos, new Vector2(pos.Value.x, pos.Value.y));
-                    if (Mathf.Abs(distance) < compareDist) {
-                        compareDist = Mathf.Abs(distance);
-                        chooseIndex = entity.Index;                        
-                    }
-                });
-            }
-            else {
-                Entities.ForEach((Entity entity, ref ReactiveComponent reactiveComp, ref Translation pos, ref TargetComponent targetComp) => {
-                    if (reactiveComp.type != EntityType.Player)
-                        return;
+                if (compareType != EntityType.Player && reactiveComp.type != EntityType.Player)
+                    return;
 
-                    if (compareIndex == entity.Index)
-                        return;
-
-                    float distance = Vector2.Distance(comparePos, new Vector2(pos.Value.x, pos.Value.y));
-                    if (Mathf.Abs(distance) < compareDist) {
-                        compareDist = Mathf.Abs(distance);
-                        chooseIndex = entity.Index;
-                    }
-                });
-            }
+                // todo : check boundary or check look direction
+                float distance = Vector2.Distance(comparePos, new Vector2(pos.Value.x, pos.Value.y));
+                if (Mathf.Abs(distance) < compareDist) {
+                    compareDist = Mathf.Abs(distance);
+                    chooseIndex = entity.Index;
+                }
+            });
 
             baseTargetComp.targetIndex = chooseIndex;
         });

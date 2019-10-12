d108c6540d0dea72e874175897c3244c0e3652c3
Joo <fizz404@naver.com>
2019-10-12 11:17:09 +0900

    Wall 타입 추가

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright 2018-2019 TAP, Inc. All Rights Reserved.
 
 namespace GlobalDefine {
-    public enum EntityType { None, Player, NonePlayer }
+    public enum EntityType { None, Player, NonePlayer, Wall }
     public enum AnimationType { Walk, Idle, SomethingDoIt, NyoNyo }
 }

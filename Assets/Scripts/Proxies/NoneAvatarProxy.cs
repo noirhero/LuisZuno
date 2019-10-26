e093b9b81311f4d43bc399e637502bc45f10b52b
hjkim <khj00767@naver.com>
2019-10-26 12:40:34 +0900

    dropItem id만 갖도롥 수정

--- Assets/Scripts/Proxies/NoneAvatarProxy.cs
+++ Assets/Scripts/Proxies/NoneAvatarProxy.cs
@@ -6,11 +6,10 @@ using GlobalDefine;
 
 [RequiresEntityConversion]
 public class NoneAvatarProxy : EntityProxy {
-    public ItemStruct dropItem;
+    public Int64 dropItem;
     public NoneAvatarStatusComponent Status;
     public override void SetupComponents(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
         base.SetupComponents(entity, dstManager, conversionSystem);
-
-        dstManager.AddComponentData(entity, new ItemComponet(dropItem));
+        
         dstManager.AddComponentData(entity, new NoneAvatarStatusComponent(ref Status));    }
 }

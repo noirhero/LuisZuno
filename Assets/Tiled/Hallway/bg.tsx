fb5df88dabbd3932b8f91efe586feb4dd6fadfc7
nhlee <noirhero83@gmail.com>
2019-12-14 12:10:00 +0900

    지하실 내려가는 계단 이미지 추가.

--- Assets/Tiled/Hallway/bg.tsx
+++ Assets/Tiled/Hallway/bg.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="bg" tilewidth="256" tileheight="240" tilecount="5" columns="0">
+<tileset version="1.2" tiledversion="1.3.1" name="bg" tilewidth="256" tileheight="240" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="256" height="240" source="backgrounds/background-pattern-1.png"/>
@@ -10,10 +10,10 @@
  <tile id="2">
   <image width="256" height="240" source="backgrounds/background-pattern-3.png"/>
  </tile>
- <tile id="3">
-  <image width="256" height="240" source="backgrounds/background-pattern-empty-bookcase.png"/>
- </tile>
  <tile id="4">
   <image width="256" height="240" source="backgrounds/background-pattern-window.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="240" source="backgrounds/background-pattern-empty-bookcase.png"/>
  </tile>
 </tileset>

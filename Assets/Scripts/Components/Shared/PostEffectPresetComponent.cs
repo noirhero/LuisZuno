c299fd380e65c89c607d53f443b640ef09ffd79d
nhlee <noirhero83@gmail.com>
2019-12-28 11:48:00 +0900

    PostEffect 엔티티 생성 기능 추가.

---
+++ Assets/Scripts/Components/Shared/PostEffectPresetComponent.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2018-2019 TAP, Inc. All Rights Reserved.
+
+using System;
+using Unity.Entities;
+using UnityEngine.Rendering.PostProcessing;
+
+public struct PostEffectPresetComponent : ISharedComponentData, IEquatable<PostEffectPresetComponent> {
+    public PostProcessVolume volume;
+
+    public bool Equals(PostEffectPresetComponent other) {
+        return other.volume == volume;
+    }
+
+    public override int GetHashCode() {
+        return (null == volume) ? 0 : volume.GetHashCode();
+    }
+}

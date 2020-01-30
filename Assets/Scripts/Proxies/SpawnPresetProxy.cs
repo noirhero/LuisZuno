76a504703d58d809c86c174cf4d3af9b40ad1c4c
nhlee <noirhero83@gmail.com>
2020-01-30 15:53:33 +0900

    스폰 시스템도 새로운 랜더링 시스템으로 변경.

--- Assets/Scripts/Proxies/SpawnPresetProxy.cs
+++ Assets/Scripts/Proxies/SpawnPresetProxy.cs
@@ -3,19 +3,36 @@
 using System.Linq;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class SpawnPresetProxy : MonoBehaviour, IConvertGameObjectToEntity {
-    public SpritePreset preset = null;
+    public NewSpritePreset preset = null;
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
         if (null == preset) {
             Debug.LogError("Set preset, now!!!!!!");
             return;
         }
-        dstManager.AddSharedComponentData(entity, new SpritePresetComponent(preset));
+        
+        var sprite = GetComponent<SpriteRenderer>()?.sprite;
+        if (ReferenceEquals(null, sprite)) {
+            Debug.LogError("Add 'SpriteRenderer' component, now!!!!!");
+            dstManager.DestroyEntity(entity);
+            return;
+        }
+        var spriteScale = new float3(sprite.rect.width, sprite.rect.height, 1.0f) / sprite.pixelsPerUnit;
+        spriteScale *= GetComponent<Transform>().localScale;
+        spriteScale.z = 1.0f;
+        dstManager.AddComponentData(entity, new NonUniformScale() {
+            Value = spriteScale
+        });
 
-        dstManager.AddComponentData(entity, new SpriteAnimComponent() {
-            nameHash = preset.datas.Keys.First()
+        dstManager.AddSharedComponentData(entity, new NewSpritePresetComponent() {
+            preset = preset
+        });
+        dstManager.AddComponentData(entity, new SpriteStateComponent() {
+            hash = preset.datas.Keys.First()
         });
     }
 }

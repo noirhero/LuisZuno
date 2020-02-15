5dbd4b45788ee46ca858a25e96da1f53106d1d59
hjkim <khj00767@naver.com>
2020-02-15 11:55:07 +0900

    npc 이동을 위한 작업, 메인씬정리

--- Assets/Scripts/Proxies/NPCProxy.cs
+++ Assets/Scripts/Proxies/NPCProxy.cs
@@ -1,14 +1,15 @@
 ﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
 
 using Unity.Entities;
-using GlobalDefine;
 using UnityEngine.Serialization;
 
 [RequiresEntityConversion]
 public class NPCProxy : EntityProxy {
+    [FormerlySerializedAs("Status")] public float speed;
+
     protected override void SetupComponents(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
         base.SetupComponents(entity, dstManager, conversionSystem);
 
-        dstManager.AddComponentData(entity, new NPCComponent(-1.0f));
+        dstManager.AddComponentData(entity, new NPCComponent(1.0f, speed));
     }
 }

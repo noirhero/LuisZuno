b3bb7d733adab8d32712783e83b79c18096c9280
hjkim <khj00767@naver.com>
2020-02-15 12:17:44 +0900

    npc 방향에 따른 이미지 반전 작업

--- Assets/Scripts/Systems/SpriteTransformSystem.cs
+++ Assets/Scripts/Systems/SpriteTransformSystem.cs
@@ -27,7 +27,7 @@ public class SpriteTransformSystem : JobComponentSystem {
             .WithName("NPCSpriteTransformSystem_AndReverse")
             .WithBurst(FloatMode.Default, FloatPrecision.Standard, true)
             .ForEach((ref LocalToWorld transform, in SpritePivotComponent pivot, in NPCComponent npc, in Translation pos, in Rotation rot, in NonUniformScale scale) => {
-                var spriteInverseRot = quaternion.RotateY(0.0f < npc.npcDirection ? math.radians(180.0f) : 0.0f);
+                var spriteInverseRot = quaternion.RotateY(0.0f < npc.npcDirection ? 0.0f : math.radians(180.0f));
                 var rotation = math.mul(spriteInverseRot, rot.Value);
                 transform.Value = float4x4.TRS(pos.Value, rotation, scale.Value);
 

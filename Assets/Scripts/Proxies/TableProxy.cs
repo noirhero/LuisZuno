ffa43dfe47250432a0f94648915faa3551cc3e99
hjkim <khj00767@naver.com>
2020-03-21 11:36:17 +0900

    Table presetPath으로 변경

--- Assets/Scripts/Proxies/TableProxy.cs
+++ Assets/Scripts/Proxies/TableProxy.cs
@@ -1,15 +1,51 @@
 ﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
 
 using UnityEngine;
+using UnityEditor;
 using Unity.Entities;
+using System.Collections.Generic;
 
 [RequiresEntityConversion]
-public class TableProxy : MonoBehaviour, IConvertGameObjectToEntity {
-    public TablePreset preset = null;
+public class TableProxy : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity {
+    public string presetPath;
+
+    protected TablePreset _preset = null;
+    
+
+    public void Awake() {
+        LoadAssets();
+    }
+
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPresets) {
+        SetupPrefabs(referencedPresets);
+    }
+
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
-        if (null != preset) {
-            dstManager.AddSharedComponentData(entity, new TablePresetComponent(preset));
+        SetupComponents(entity, dstManager, conversionSystem);
+    }
+
+
+    protected virtual void LoadAssets() {
+        if (0 < presetPath.Length) {
+            _preset = AssetDatabase.LoadAssetAtPath<TablePreset>(presetPath);
         }
+    }
+
+
+    protected virtual void SetupPrefabs(List<GameObject> referencedPresets) {
+        if (null != _preset) {
+            referencedPresets.Add(_preset.gameObject);
+        }
+    }
+
+
+    protected virtual void SetupComponents(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+        if (null == _preset) {
+            return;
+        }
+
+        dstManager.AddSharedComponentData(entity, new TablePresetComponent(_preset));
     }
 }

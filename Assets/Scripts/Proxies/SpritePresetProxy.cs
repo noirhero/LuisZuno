57697b2d45cf60da9c8ff05d9574fde6a626f074
tbuba <tbuba@gameparadiso.com>
2020-04-23 09:41:00 +0900

    SpritePreset 찾기 시스템 추가.

---
+++ Assets/Scripts/Proxies/SpritePresetProxy.cs
@@ -0,0 +1,30 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class SpritePresetProxy : MonoBehaviour, IConvertGameObjectToEntity {
+    public int guid;
+
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+        var spritePreset = GetComponent<SpritePreset>();
+        if (ReferenceEquals(null, spritePreset)) {
+            Debug.LogError("Add 'SpritePreset' now!!!!!");
+            dstManager.DestroyEntity(entity);
+            return;
+        }
+
+        dstManager.RemoveComponent<LocalToWorld>(entity);
+        dstManager.RemoveComponent<Translation>(entity);
+        dstManager.RemoveComponent<Rotation>(entity);
+
+        dstManager.AddSharedComponentData(entity, new SpritePresetGuidComponent() {
+            guid = guid,
+            preset = spritePreset
+        });
+    }
+}
---
+++ Assets/Scripts/Systems/FindSpritePresetSystem.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 2018-2020 TAP, Inc. All Rights Reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Entities;
+
+public class FindSpritePresetSystem : ComponentSystem {
+    private Dictionary<int, SpritePreset> _spritePresets = new Dictionary<int, SpritePreset>();
+    protected override void OnStartRunning() {
+        List<Entity> destroyEntites = new List<Entity>();
+        Entities.ForEach((Entity entity, SpritePresetGuidComponent guidComp) => {
+            destroyEntites.Add(entity);
+            _spritePresets.Add(guidComp.guid, guidComp.preset);
+        });
+
+        foreach (var entity in destroyEntites) {
+            EntityManager.DestroyEntity(entity);
+        }
+    }
+
+    protected override void OnUpdate() {
+        Entities.ForEach((Entity entity, ref FindSpritePresetComponent findComp) => {
+            if (false == _spritePresets.TryGetValue(findComp.guid, out var spritePreset)) {
+                return;
+            }
+
+            EntityManager.AddSharedComponentData(entity, new SpritePresetComponent() {
+                preset = spritePreset
+            });
+            EntityManager.AddComponentData(entity, new SpriteStateComponent() {
+                hash = spritePreset.datas.Keys.First()
+            });
+            EntityManager.RemoveComponent<FindSpritePresetComponent>(entity);
+        });
+    }
+}
